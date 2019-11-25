5d87e5abc3ee4bfd8917e3cb8fe8768f7b40e357
oubin <oubin@beisen.com>
2019-11-25 15:02:58 +0800

    Update ActionFilterExtend.cs

--- CoreAPI/Filters/ActionFilterExtend.cs
+++ CoreAPI/Filters/ActionFilterExtend.cs
@@ -66,30 +66,4 @@ namespace Filters
             return base.OnResultExecutionAsync(context, next);
         }
     }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public class ExceptionFilterExtend : IExceptionFilter
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="context"></param>
-        public void OnException(ExceptionContext context)
-        {
-            // 日志入库
-            //向负责人发报警邮件，异步
-            //向负责人发送报警短信或者报警电话，异步
-
-            Exception ex = context.Exception;
-            //这里获取服务器ip时，需要考虑如果是使用nginx做了负载，这里要兼容负载后的ip，
-            //监控了ip方便定位到底是那台服务器出故障了
-            //string ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
-
-            LogHelper.Error($"堆栈信息：{ex.StackTrace},异常描述：{ex.Message}");
-            context.Result = new JsonResult(new OutputModel<string> { StatusCode = (int)HttpStatusCode.InternalServerError, IsSuccess = false, Message = "内部错误", Data = "" });
-            context.ExceptionHandled = true;
-        }
-    }
 }
